ded5051f335ebeff249908a55cea7f0379fc2591
guest271314 <guest271314@gmail.com>
2025-04-26 05:14:33 +0000

    Handle 65536 input/output, multiple requests and connections
    - Handle 65526 ArrayBuffer byteLength input and ouput
    - Handle multiple requests and connections

--- assets/script.js
+++ assets/script.js
@@ -1,50 +1,70 @@
-// TODO: TLS, HTTP/2
-// Copyright (c) 2019-2021 Robert Rypuła - https://github.com/robertrypula
-// Fork of https://gist.github.com/robertrypula/b813ffe23a9489bae1b677f1608676c8
-const debugBuffer = (bufferName, buffer) => {
-  const length = buffer ? buffer.length : "---";
-  console.log(`:: DEBUG - ${bufferName} | ${length} | `, buffer, "\n");
-};
+// https://github.com/lsert/websocketparser
+function wsparser(d) {
+  const tArr = d;
+  const group0 = tArr[0];
+  const FIN = group0 >> 7;
+  const RSV1 = (group0 & 64) >> 6;
+  const RSV2 = (group0 & 32) >> 5;
+  const RSV3 = (group0 & 16) >> 4;
+  const opcode = group0 & 15;
 
-/*
-  https://tools.ietf.org/html/rfc6455#section-5.2
-    0                   1                   2                   3
-    0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-   +-+-+-+-+-------+-+-------------+-------------------------------+
-   |F|R|R|R| opcode|M| Payload len |    Extended payload length    |
-   |I|S|S|S|  (4)  |A|     (7)     |             (16/64)           |
-   |N|V|V|V|       |S|             |   (if payload len==126/127)   |
-   | |1|2|3|       |K|             |                               |
-   +-+-+-+-+-------+-+-------------+ - - - - - - - - - - - - - - - +
-   |     Extended payload length continued, if payload len == 127  |
-   + - - - - - - - - - - - - - - - +-------------------------------+
-   |                               |Masking-key, if MASK set to 1  |
-   +-------------------------------+-------------------------------+
-   | Masking-key (continued)       |          Payload Data         |
-   +-------------------------------- - - - - - - - - - - - - - - - +
-   :                     Payload Data continued ...                :
-   + - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - +
-   |                     Payload Data continued ...                |
-   +---------------------------------------------------------------+
-  OpCode
-    %x0 denotes a continuation frame
-    %x1 denotes a text frame
-    %x2 denotes a binary frame
-    %x3–7 are reserved for further non-control frames
-    %x8 denotes a connection close
-    %x9 denotes a ping
-    %xA denotes a pong
-    %xB-F are reserved for further control frames
-*/
+  const group1 = tArr[1];
+  const group2 = tArr[2];
+  const group3 = tArr[3];
 
-// ---------------------------------------------------------
+  // 计算数据开始点
+  let counter = 2;
 
+  const mask = group1 >> 7;
+  const payloadLens = group1 & 127;
+  let realLens = payloadLens;
+  if (payloadLens === 126) {
+    realLens = (group2 << 8) | group3;
+    counter += 2;
+  } else if (payloadLens === 127) {
+    const ab = new ArrayBuffer(8);
+    const dv = new DataView(ab);
+    for (let i = 0; i < 8; i++) {
+      dv.setUint8(i, tArr[i + 2]);
+    }
+    realLens = dv.getBigUint64(0, false);
+    counter += 8;
+  }
+
+  let data;
+  if (mask === 1) {
+    const maskKeyArr = tArr.slice(counter, counter + 4);
+    counter += 4;
+
+    // 解掩码
+    data = tArr.slice(counter).map((item, index) => {
+      const j = index % 4;
+      return item ^ maskKeyArr[j];
+    });
+  } else {
+    data = tArr.slice(counter);
+  }
+  let arr = {
+    FIN,
+    mask,
+    RSV1,
+    RSV2,
+    RSV3,
+    opcode,
+    realLens,
+    data: new Uint8Array(data),
+  };
+  return arr;
+}
+
+// https://gist.github.com/robertrypula/b813ffe23a9489bae1b677f1608676c8
 const createWebSocketFrame = (payload) => {
   const payloadLengthByteCount = payload.length < 126 ? 0 : 2;
   const buffer = new ArrayBuffer(2 + payloadLengthByteCount + payload.length);
   const view = new DataView(buffer);
   let payloadOffset = 2;
-
+  console.log(payload.length, payloadLengthByteCount);
+  // TODO: Handle greater than 65536 input ArrayBuffer byteLength (guest271314)
   if (payload.length >= Math.pow(2, 16)) {
     throw new Error("Payload equal or bigger than 64 KiB is not supported");
   }
@@ -52,7 +72,7 @@ const createWebSocketFrame = (payload) => {
   view.setUint8(0, 0b10000010);
   view.setUint8(1, payload.length < 126 ? payload.length : 126);
   if (payloadLengthByteCount > 0) {
-    view.setUint16(2, payload.length);
+    view.setUint16(2, payload.length, false);
     payloadOffset += payloadLengthByteCount;
   }
 
@@ -62,125 +82,12 @@ const createWebSocketFrame = (payload) => {
   return buffer;
 };
 
-// ---------------------------------------------------------
-
-const getParsedBuffer = (buffer) => {
-  console.log({ buffer });
-  const view = new DataView(buffer.buffer);
-  let bufferRemainingBytes;
-  let currentOffset = 0;
-  let maskingKey;
-  let payload;
-
-  if (currentOffset + 2 > buffer.length) {
-    return { payload: null, bufferRemainingBytes: buffer };
-  }
-
-  const firstByte = view.getUint8(currentOffset++);
-  const secondByte = view.getUint8(currentOffset++)
-  const isFinalFrame = !!((firstByte >>> 7) & 0x1);
-  const opCode = firstByte & 0xf;
-  const isMasked = !!((secondByte >>> 7) & 0x1); // https://security.stackexchange.com/questions/113297
-  let payloadLength = secondByte & 0x7f;
-
-  if (!isFinalFrame) {
-    console.log("[not final frame detected]\n");
-  }
-
-  if (opCode === 0x8) {
-    console.log("[connection close frame]\n");
-    // TODO read payload, for example payload equal to <0x03 0xe9> means 1001:
-    //   1001 indicates that an endpoint is "going away", such as a server
-    //   going down or a browser having navigated away from a page.
-    // More info here: https://tools.ietf.org/html/rfc6455#section-7.4
-    return { payload: null, bufferRemainingBytes: null };
-  }
-
-  if (opCode !== 0x2 && opCode !== 0x0) {
-    throw new Error("Only binary and continuation frames are supported");
-  }
-
-  if (payloadLength > 125) {
-    if (payloadLength === 126) {
-      if (currentOffset + 2 > buffer.length) {
-        return { payload: null, bufferRemainingBytes: buffer };
-      }
-      payloadLength = view.getUint16(currentOffset);
-      currentOffset += 2;
-    } else {
-      throw new Error("Payload equal or bigger than 64 KiB is not supported");
-    }
-  }
-
-  if (isMasked) {
-    if (currentOffset + 4 > buffer.length) {
-      return { payload: null, bufferRemainingBytes: buffer };
-    }
-    maskingKey = view.getUint32(currentOffset);
-    currentOffset += 4;
-  }
-
-  if (currentOffset + payloadLength > buffer.length) {
-    console.log("[misalignment between WebSocket frame and NodeJs Buffer]\n");
-    return { payload: null, bufferRemainingBytes: buffer };
-  }
-
-  payload = new Uint8Array(payloadLength);
-
-  if (isMasked) {
-    for (let i = 0, j = 0; i < payloadLength; ++i, j = i % 4) {
-      const shift = j === 3 ? 0 : (3 - j) << 3;
-      const mask = (shift === 0 ? maskingKey : maskingKey >>> shift) & 0xff;
-
-      payload[i] = mask ^ view.getUint8(currentOffset++);
-    }
-  } else {
-    for (let i = 0; i < payloadLength; i++) {
-      payload[i] = view.getUint8(currentOffset++);
-    }
-  }
-
-  bufferRemainingBytes = new Uint8Array(buffer.length - currentOffset);
-  for (let i = 0; i < bufferRemainingBytes.length; i++) {
-    bufferRemainingBytes[i] = view.getUint8(currentOffset++);
-  }
-
-  return { payload, bufferRemainingBytes };
-};
-
-function parseWebSocketFrame(buffer) {
-  let bufferToParse = buffer;
-  let parsedBuffer;
-  do {
-    parsedBuffer = getParsedBuffer(bufferToParse);
-    debugBuffer("buffer", buffer);
-    debugBuffer("bufferToParse", bufferToParse);
-    debugBuffer("parsedBuffer.payload", parsedBuffer.payload);
-    debugBuffer(
-      "parsedBuffer.bufferRemainingBytes",
-      parsedBuffer.bufferRemainingBytes,
-    );
-    if (parsedBuffer.payload === null) {
-      return parsedBuffer.payload;
-    }
-    bufferToParse = parsedBuffer.bufferRemainingBytes;
-
-    if (parsedBuffer.payload) {
-      console.log(parsedBuffer);
-      break;
-    }
-  } while (parsedBuffer.payload && parsedBuffer.bufferRemainingBytes.length);
-  return createWebSocketFrame(parsedBuffer.payload);
-}
-
+// Handle WebSocket handshake
 // https://stackoverflow.com/a/77398427
 async function digest(message, algo = "SHA-1") {
-  const { promise, resolve } = Promise.withResolvers();
-  const reader = new FileReader();
-  reader.onload = () => resolve(reader.result);
-  reader.readAsDataURL(
-    new Blob([
-      new Uint8Array(
+  return btoa(
+    [
+      ...new Uint8Array(
         await crypto.subtle.digest(
           algo,
           new TextEncoder().encode(
@@ -188,27 +95,35 @@ async function digest(message, algo = "SHA-1") {
           ),
         ),
       ),
-    ]),
+    ].map((s) => String.fromCodePoint(s)).join(""),
   );
-  const result = await promise;
-  return result.split(",").pop();
 }
 // Get Request-Line and Headers
+// TODO: Get request line URI, protocol
 function getHeaders(r) {
-  console.log(r);
   const header = r.match(/.+/g).map((line) => line.split(/:\s|\s\/\s/));
   const [requestLine] = header.shift();
   const [method, uri, protocol] = requestLine.split(/\s/);
   const headers = new Headers(header);
-  console.log({method, uri, protocol, headers});
+  console.log({
+    method,
+    uri,
+    protocol,
+    headers,
+  });
   const url = new URL(uri, `http://${headers.get("host")}`);
   const { servertype } = Object.fromEntries(url.searchParams);
-  return { headers, uri, protocol, servertype }; 
+  return {
+    headers,
+    uri,
+    protocol,
+    servertype,
+  };
 }
 // IWA window
 onload = async () => {
   resizeTo(300, 200);
-  const USER_AGENT = "";
+  const USER_AGENT = "Built with Deno/2.3.0-rc.0+5bee292";
   console.log(USER_AGENT);
 
   const encoder = new TextEncoder();
@@ -216,68 +131,111 @@ onload = async () => {
   const encode = (text) => encoder.encode(text);
 
   const socket = new TCPServerSocket("0.0.0.0", {
-    localPort: 44818, // EtherNet/IP
+    localPort: 44818,
+    // EtherNet/IP
   });
 
-  const {
-    readable: server,
-    localAddress,
-    localPort,
-  } = await socket.opened;
+  const { readable: server, localAddress, localPort } = await socket.opened;
+  const requests = [];
+  const abortable = new AbortController();
+  const { signal } = abortable;
+  Object.assign(globalThis, { socket, server, abortable, requests });
 
-  console.log({ server });
+  console.log({
+    socket,
+    server,
+  });
+
+  document.body.textContent = JSON.stringify(
+    {
+      localAddress,
+      localPort,
+    },
+    null,
+    2,
+  );
   // TODO:
   // Handle multiple connections
   await server.pipeTo(
     new WritableStream({
       async write(connection) {
-        const {
-          readable: client,
-          writable,
-          remoteAddress,
-          remotePort,
-        } = await connection.opened;
-        console.log({ connection });
-        document.body.textContent = JSON.stringify(
-          { localAddress, localPort, remoteAddress, remotePort },
-            null,
-            2,
-          );
+        const { readable: client, writable, remoteAddress, remotePort } =
+          await connection.opened;
+        globalThis.requests.push({ remoteAddress, remotePort, client });
+        console.log({
+          connection,
+          requests,
+        });
+
         const writer = writable.getWriter();
+        globalThis.writer = writer;
+
         console.log({
           remoteAddress,
           remotePort,
         });
 
-        const abortable = new AbortController();
-        const { signal } = abortable;
-        // Text streaming
-        // .pipeThrough(new TextDecoderStream())
-        await client.pipeTo(
+        // Don't await to handle multiple requests
+        client.pipeTo(
           new WritableStream({
-            start(controller) {             
+            start: (controller) => {
               console.log(controller);
+              this.fragments = [];
             },
-            async write(r, controller) {
+            write: async (r, controller) => {
               const request = decoder.decode(r);
-              console.log(request);
+              // console.log(request);
               // Handle WebSocket
               if (!/(GET|POST|HEAD|OPTIONS|QUERY)/i.test(request)) {
-                console.log({ data: r });
-                const response = parseWebSocketFrame(r);
-                console.log({ response });
-                if (response === null) {
-                  await writer.write(new Uint8Array([0x88, 0x00])); // 136, 0
+                let parsed = wsparser(r);
+                console.log(
+                  {
+                    r,
+                  },
+                  parsed.data.length,
+                  parsed.realLens,
+                );
+                // Handle fragmented frames up to 65536 byteLength
+                // from input ArrayBuffer
+                if (parsed.data.length < parsed.realLens) {
+                  this.fragments.push(parsed.data);
+                  console.log(this.fragments);
+                  return;
+                }
+                if (this.fragments.length) {
+                  const data = [];
+                  do {
+                    for (let i = 0; i < this.fragments.length; i++) {
+                      data.push(...this.fragments.shift());
+                    }
+                  } while (this.fragments.length);
+                  parsed.data = new Uint8Array([...data, ...r]);
+                  console.log({
+                    parsed,
+                  });
+                }
+                // Handle client closing WebSocket
+                if (parsed.opcode === 0x8 && parsed.data.length === 0) {
+                  console.log(parsed.opcode, parsed.data);
+                  await writer.write(new Uint8Array([0x88, 0x00]));
+                  // 136, 0
                   await writer.close();
                   return await writer.closed;
-                  // return abortable.abort("WebSocket closed by client, aborted in server");
                 }
-                await writer.write(response);
+                // Write 16384 bytes
+                for (let i = 0; i < parsed.data.length; i += 16384) {
+                  await writer.write(
+                    createWebSocketFrame(parsed.data.subarray(i, i + 16384)),
+                  );
+                }
                 // Don't close WebSocket
                 // await writer.close();
               }
+              // Handle WebSocket request
               if (/^GET/.test(request) && /websocket/i.test(request)) {
-                const { headers, method, uri, servertype } = getHeaders(request);
+                const { headers, method, uri, servertype } = getHeaders(
+                  request,
+                );
                 const key = headers.get("sec-websocket-key");
                 const accept = await digest(key);
                 await writer.write(
@@ -311,6 +269,9 @@ onload = async () => {
                 );
                 await writer.close();
               }
+              // Handle POST, QUERY requests
+              // Respond with Transfer-Encoding Content-Type
+              // for uni-directional server to client streaming over HTTP/1.1
               if (/^(POST|query)/i.test(request)) {
                 const [body] = request.match(
                   /(?<=\r\n\r\n)[a-zA-Z\d\s\r\n-:;=]+/igm,
@@ -344,23 +305,7 @@ onload = async () => {
                 await writer.write(encode(`${size}\r\n`));
                 await writer.write(chunk.buffer);
                 await writer.write(encode("\r\n"));
-                /*
-                const response = await fetch("https://gist.githubusercontent.com/guest271314/73a50e9ebc6acaaff5d39f6fc7918ebf/raw/85bce58862fcf905ce7beba1a837a5c778ba8e47/http_over_tcp_browser_server.md");
-                await response.body.pipeTo(
-                  new WritableStream({
-                    async write(chunk) {
-                      const size = chunk.buffer.byteLength.toString(16);
-                      // console.log(chunk.buffer.byteLength, size);
-                      await writer.write(encode(`${size}\r\n`));
-                      await writer.write(chunk.buffer);
-                      await writer.write(encode("\r\n"));
-                    },
-                    close() {
-                      console.log("Stream closed");
-                    },
-                  }),
-                );
-                */
+
                 await writer.write(encode("0\r\n"));
                 await writer.write(encode("\r\n"));
                 await writer.close();
@@ -372,8 +317,8 @@ onload = async () => {
             abort(reason) {
               console.log(reason);
             },
-          })
-        , {signal}).catch(console.warn);
+          }),
+        ).catch(console.warn);
       },
       close() {
         console.log("Host closed");
@@ -382,5 +327,8 @@ onload = async () => {
         console.log("Host aborted", reason);
       },
     }),
+    {
+      signal,
+    },
   ).then(() => console.log("Server closed")).catch(console.warn);
 };
