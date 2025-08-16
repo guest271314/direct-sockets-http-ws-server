abf2f50e17dfc30e03ce6f96b1820214fb6e1cf5
guest271314 <guest271314@gmail.com>
2025-08-16 19:39:17 +0000

    Optimize get-chunked-data.js

--- assets/get-chunked-data.js
+++ assets/get-chunked-data.js
@@ -1,13 +1,18 @@
 function getChunkedData(u8) {
   let crlfIndex = 0;
-  let chunkLength = "";
+  let chunkLength = 0;
   let chunkBuffer = new Uint8Array(0);
   crlfIndex = u8.findIndex((v, k, array) => v === 13 && array[k + 1] === 10);
   if (crlfIndex > -1) {
-    chunkLength = parseInt(
-      String.fromCodePoint(...u8.subarray(0, crlfIndex)),
-      16,
-    );
+    // https://gist.github.com/guest271314/d6e932154e11fffb75fd7d1a4b25f4f5
+    for (let i = 0; i < crlfIndex; i++) {
+      const hex = u8[i];
+      let intVal = hex & 0xF;
+      if (hex > 64) { 
+        intVal += 9;
+      }
+      chunkLength = intVal + (chunkLength << 4);
+    }
     chunkBuffer = u8.subarray(crlfIndex + 2, crlfIndex + chunkLength + 2);
     crlfIndex += chunkLength + 4;
   }
