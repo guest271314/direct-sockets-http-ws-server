ea19c691d6903dc90262c978e0cec62a324c5e1a
guest271314 <guest271314@gmail.com>
2025-08-20 13:41:09 +0000

    Update ws.js
    Write 32768 bytes at a time, for reliability, to avoid occasionally receiving 10420224 echoed instead of 10485760 when writing 10 MB. Investigate the occasional missing 65536...

--- examples/ws.js
+++ examples/ws.js
@@ -5,65 +5,63 @@ function setTitle(data) {
 }
 
 if (globalThis?.openIsolatedWebApp) {
-  openIsolatedWebApp(`?name=TCPServerSocket`);
+  await openIsolatedWebApp(`?name=TCPServerSocket`);
 } else {
   setTitle(`?name=TCPServerSocket`);
 }
+
 await scheduler.postTask(() => {}, {
-  delay: 3000,
+  delay: 4000,
   priority: "user-visible",
 });
 
+var u8 = new Uint8Array(1024 ** 2 * 10).fill(1);
 var decoder = new TextDecoder();
 var encoder = new TextEncoder();
 
 var wss = new WebSocketStream("ws://127.0.0.1:44818");
 console.log(wss);
 
-var {readable, writable} = await wss.opened.catch(console.warn);
+var { readable, writable } = await wss.opened.catch(console.warn);
 
 var reader = readable.getReader();
 var writer = writable.getWriter();
 
-Promise.allSettled([writable.closed, readable.closed, wss.closed])
-  .then( ([,,{value: {closeCode, reason}}]) => console.log({
-  closeCode,
-  reason
-}));
+Promise.allSettled([writable.closed, readable.closed, wss.closed]).then((
+  args,
+) => console.log(args));
 
 async function write(data) {
-  const len = 65536;
+  const len = 65536 / 2;
   let bytes = 0;
+
   if (typeof data === "string") {
     for (let i = 0; i < data.length; i += len) {
       await writer.ready;
       await writer.write(data.slice(i, i + len));
-      const {value, done} = await reader.read();
-      console.log(value, bytes += value.length);
+      const { value, done } = await reader.read();
+      bytes += value.length;
     }
   } else {
-    for await(const value of new Response(data).body) {
+    for await (const value of new Response(data).body) {
       for (let i = 0; i < data.length; i += len) {
+        const d = data.subarray(i, i + len);
         await writer.ready;
-        await writer.write(data.subarray(i, i + len));
-        const {value, done} = await reader.read();
-        console.log(value, bytes += value.byteLength);
+        await writer.write(d);
+        const { value, done } = await reader.read();
+        bytes += value.byteLength;
       }
     }
   }
-  // Read until done
-  // This hangs. read() fulfills before writer.close(), possible race condition
-  // return reader.read().then(({value, done}) => {
-  //   console.log(value, done);
-  //   return bytes;
-  // });
   reader.read().then(console.log);
   return bytes;
 }
 
-var x = await write(new Uint8Array(1024 ** 2 * 10));
+var x = await write(u8);
 // var z = await write("x".repeat(1024**2));
-
 console.log(x);
 
+await writer.ready;
 await writer.close();
+await writer.closed.then(() => console.log("writer closed"));
+await reader.closed.then(() => console.log("reader closed"));
