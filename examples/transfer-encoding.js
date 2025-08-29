38e017cf02fac57732b70540b4de4570d6934047
guest271314 <guest271314@gmail.com>
2025-08-29 04:11:54 +0000

    Update transfer-encoding.js

--- examples/transfer-encoding.js
+++ examples/transfer-encoding.js
@@ -2,44 +2,35 @@ var abortable = new AbortController();
 
 var { readable, writable } = new TransformStream({
   async transform(v, c) {
-    for (let i = 0; i < v.length; i+= 8192) {
+    for (let i = 0; i < v.length; i += 8192) {
+      if (abortable.signal.aborted) {
+        c.terminate();
+        break;
+      }
       c.enqueue(v.subarray(i, i + 8192));
-      await scheduler.postTask(() => {}, {delay:30});
+      await scheduler.postTask(() => {}, { delay: 30 });
     }
-  }, 
+  },
   flush() {
     console.log("flush");
-    abortable.abort("");
-  }
-}, {highWaterMark:1});
+  },
+});
 var writer = writable.getWriter();
 var response = fetch("http://localhost:44818", {
   method: "post",
   duplex: "half",
   body: readable,
   signal: abortable.signal,
-  allowHTTP1ForStreamingUpload: true
-}).then((r) => {
-  console.log(...r.headers);
-  return r.body.pipeTo(
-    new WritableStream({
-      write(v) {
-        console.log(v);
-      },
-      close() {
-        console.log("close");
-      }
-    })
-  )
-})
+  allowHTTP1ForStreamingUpload: true,
+}).then((r) => r.text())
   .catch((e) => {
-    console.log(e);
-  })
-.then(() => {
-  console.log("Done streaming");
-})
-.catch(console.log);
-await scheduler.postTask(() => {}, {delay:45});
-await writer.write(new Uint8Array(1024**2*5).fill(1));
-await writer.ready;
-await writer.close();
+    return e;
+  });
+await scheduler.postTask(() => {}, { delay: 45 });
+await writer.write(new Uint8Array(1024 ** 2 * 7).fill(1));
+await writer.ready
+  .then(() => writer.close())
+  .catch(() => {});
+await response.then((response) => {
+  console.log({ response });
+});
